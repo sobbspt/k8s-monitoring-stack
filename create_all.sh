ccfe9ac4612370c6cc47ef92060dbee0bfd64bd6
Sobb <kitaboons@gmail.com>
2020-09-16 00:56:16 +0700

    Init

---
+++ create_all.sh
@@ -0,0 +1,17 @@
+# Create Elastic cluster
+helm repo add elastic https://helm.elastic.co
+helm upgrade --install elasticsearch  elastic/elasticsearch -f elasticsearch/values.yaml --version 7.9.1  
+
+# Create Elastic metric exporter
+helm upgrade --install elasticsearch-exporter stable/elasticsearch-exporter -f elasticsearch-exporter/values.yaml --version 3.7.0
+
+# Create Prometheus operator and dependencies (CRDs)
+sh ./prometheus-operation/create_crds.sh
+helm repo add prometheus-com https://prometheus-community.github.io/helm-charts
+helm upgrade --install prometheus-operator prometheus-community/kube-prometheus-stack -f prometheus-operation/values.yaml --version 9.4.2
+
+# For Grafana Elastic dashboard, this link might be useful https://grafana.com/grafana/dashboards/2322
+
+# Optional :: if nginx-ingress-controller is not installed yet.
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm install my-release ingress-nginx/ingress-nginx
